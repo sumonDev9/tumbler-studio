6afcf8482c2037d3b6a17c1b56f838414cd946e6
Sumon Mitra <sumonmit678@gmail.com>
2026-02-21 17:01:27 +0530

    Add VisionGallery model with fillable fields

--- app/Models/VisionGallery.php
+++ app/Models/VisionGallery.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
-class VisionGallery extends Model
-{
-    //
+class VisionGallery extends Model {
+    protected $fillable = ['title', 'image'];
 }
