e04b9f96470dec4cfc0bd1f64a8cb59c845e3ad0
Sumon Mitra <sumonmit678@gmail.com>
2026-02-22 12:29:46 +0530

    Add AdminContactMail Mailable with dynamic subject line

---
+++ app/Mail/AdminContactMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AdminContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contactData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($contactData)
+    {
+        $this->contactData = $contactData;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        // Subject e user er subject ta jure dewa holo
+        return $this->subject('New Contact Inquiry: ' . ($this->contactData->subject ?? 'No Subject'))
+                    ->view('emails.admin_contact');
+    }
+}
