bd23713306b9c4ad64a643349ff6045ab5e72430
Sumon Mitra <sumonmit678@gmail.com>
2026-02-22 12:28:14 +0530

    Add UserContactMail Mailable for user email notifications

---
+++ app/Mail/UserContactMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contactData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($contactData)
+    {
+        $this->contactData = $contactData;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Thank you for contacting us!')
+                    ->view('emails.user_contact');
+    }
+}
